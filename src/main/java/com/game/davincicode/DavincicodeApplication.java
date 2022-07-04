7fd4e092bbb0f91ce85e501ac88dbc0d7a89d0fa
myoungji <myoung9412@daum.net>
2022-07-04 11:28:56 +0900

    Initial commit

---
+++ src/main/java/com/game/davincicode/DavincicodeApplication.java
@@ -0,0 +1,13 @@
+package com.game.davincicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavincicodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DavincicodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/davincicode/DavincicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.davincicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavincicodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
